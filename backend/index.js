f4d85ce81c641d3222512c261e0a7f67ca8bea9a
rohit-06-nishad <rohitnishad123467@gmail.com>
2026-01-09 14:56:07 +0530

    basic backend Setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDb from "./config/connectDb.js"
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoute.js";
+import cors from "cors";
 
 // env config
 dotenv.config();
@@ -13,6 +14,10 @@ const port = process.env.PORT || 5000;
 // middleware config
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173/",
+  credentials:true
+}))
 
 app.use("/api/auth", authRouter);
 
