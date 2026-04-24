9e16bb1be8b8db2974fd011b5990a204ddc9caca
Rohit <rohitnishad123467@gmail.com>
2026-04-24 12:59:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://edunexus-frontend-23o3.onrender.com",
   credentials:true
 }))
 
